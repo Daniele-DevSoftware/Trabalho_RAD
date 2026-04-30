8295152b1c2e1efbaaf934a5eb559e559c267529
DanieleCouto <nebulosa.de.orion05@gmail.com>
2026-04-30 13:21:16 -0300

    Add function to delete student by matricula

--- CRUD/delete.py
+++ CRUD/delete.py
@@ -1,1 +1,10 @@
+def deletar_aluno(matricula):
+  conn = conectar()
+  cursor = conn.cursor()
 
+  cursor.execute("""
+  DELETE FROM alunos WHERE matricula = ?
+  """, (matricula))
+
+  conn.commit()
+  conn.close()
