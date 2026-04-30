ae06b459c14680686295137ebeb3628909f38025
DanieleCouto <nebulosa.de.orion05@gmail.com>
2026-04-30 13:14:01 -0300

    Implement buscar_nome function for name search
    Add function to search for students by name

--- CRUD/read_buscar_nome.py
+++ CRUD/read_buscar_nome.py
@@ -1,1 +1,12 @@
+def buscar_nome(matricula):
+  conn = conectar()
+  cursor = conn.cursor()
 
+  cursor.execute("""
+  SELECT * FROM alunos WHERE nome LIKE ?
+  """, ('%' + nome + '%',))
+
+  resultado = cursor.fetchall()
+
+  conn.close()
+  return resultado
