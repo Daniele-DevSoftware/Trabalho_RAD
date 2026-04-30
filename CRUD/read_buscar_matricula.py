8f1676aeea49b10daf31ad3d0e8c14527e6059f6
DanieleCouto <nebulosa.de.orion05@gmail.com>
2026-04-30 13:10:11 -0300

    Create read_buscar_matricula.py

---
+++ CRUD/read_buscar_matricula.py
@@ -0,0 +1,12 @@
+def buscar_matricula(matricula):
+  conn = conectar()
+  cursor = conn.cursor()
+
+  cursor.execute("""
+  SELECT * FROM alunos WHERE matricula = ?
+  """, (matricula))
+
+  aluno = cursor.fetchone()
+
+  conn.close()
+  return aluno
