2b8c0fcbdefb2f0aecafb1908bab5131f0acb5d1
DanieleCouto <nebulosa.de.orion05@gmail.com>
2026-04-30 12:54:27 -0300

    Update insert_alunos.py

--- CRUD/insert_alunos.py
+++ CRUD/insert_alunos.py
@@ -1,1 +1,15 @@
+def inserir_aluno(matricula, nome, data_nascimento, nota):
+  conn = conectar()
+  cursor = conn.cursor()
 
+try:
+  cursor.execute("""
+  INSERT INTO alunos (matricula, nome, data_nasciemento, nota)
+  VALUES (?, ?, ?, ?)
+  """, (matricula, nome, data_nascimento, nota))
+
+  conn.commit()
+except sqlite3.IntegrityError:
+  print("Matrícula já cadastrada!")
+
+conn.close()
