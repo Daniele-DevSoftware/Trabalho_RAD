b84dc7b32c5f10a5bc68734c24b2830773640b4c
DanieleCouto <nebulosa.de.orion05@gmail.com>
2026-04-30 13:19:35 -0300

    Update update.py

--- CRUD/update.py
+++ CRUD/update.py
@@ -1,1 +1,12 @@
+def atualizar_aluno(matricula, nome, data_nascimento, nota):
+  conn = conectar()
+  cursor = conn.cursor()
 
+  cursor.execute("""
+  UPDATE alunos
+  SET nome = ?, data_nascimento = ?, nota = ?
+  WHERE matricula = ?
+  """, (matricula, nome, data_nascimento, nota))
+
+  conn.commit()
+  conn.close()
