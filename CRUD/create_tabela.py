01a204e9bcf472b4eef84309aade9113886e9169
DanieleCouto <nebulosa.de.orion05@gmail.com>
2026-04-30 12:46:46 -0300

    Update create_tabela.py

--- CRUD/create_tabela.py
+++ CRUD/create_tabela.py
@@ -1,1 +1,15 @@
+def criar_tabela():
+  conn = conectar()
+  cursor = conn.cursor()
 
+  cursor.execute("""
+  CREATE TABLE IF NOT EXISTS alunos (
+    matricula TEXT PRIMARY KEY,
+    nome TEXT NOT NULL,
+    data_nascimento TEXT,
+    nota REAL
+  )
+  """)
+
+  conn.commit()
+  conn.close()
